26e12ec05be7a8db43f870408a2d6e15e3ffca0a
earlloyd <earllloydsaga925@gmail.com>
2024-09-06 20:00:28 +0800

    Add files via upload

---
+++ firstoop.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+    <?php
+        echo "my name is Earl Lloyd T. Saga <br>";
+        echo "I live in Camanucan <br>";
+    ?>
+</body>
+</html>
+
+
